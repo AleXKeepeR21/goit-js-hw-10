c3314fe9afe4b0b2dc6530db06894356247458b4
AleXKeepeR21 <106617654+AleXKeepeR21@users.noreply.github.com>
2022-11-06 12:28:00 +0200

    add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import './css/styles.css';
+import { fetchCountries } from './js/fetchCountries.js';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+
+const inputEl = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+// inputEl.addEventListener('input', onInput);
+
+// function onInput(evt) {
+//   // console.dir(evt.currentTarget.elements);
+
+//   // const input = evt.currentTarget..value;
+//   // console.log(input);
+
+// }
+
+fetchCountries();
