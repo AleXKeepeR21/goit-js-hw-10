f73bba122172f4c41c61034b23e5bf00615c9935
AleXKeepeR21 <106617654+AleXKeepeR21@users.noreply.github.com>
2022-11-06 21:46:34 +0200

    add css slyle, add reset fn

--- src/index.js
+++ src/index.js
@@ -11,57 +11,43 @@ const countryInfo = document.querySelector('.country-info');
 inputEl.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
 function onInput(evt) {
-  const name = evt.target.value;
-  // console.log(name);
+  resetData();
+  const coutriesSearch = evt.target.value;
 
-  fetchCountries(name).then(onMarkup);
+  fetchCountries(coutriesSearch).then(onMarkup);
 
   function onMarkup(data) {
-    // console.log(data)
-    const markup = data
+    const markupCountryList = data
       .map(
         item =>
           `<li class="item">
               <img src= ${item.flags.svg} alt="">
               <p class="name">${item.name.official}</p>
-              <p class="capital">${item.capital}</p>
-              <p class="population">${item.population}</p>
-              <p class="languages">${Object.values(item.languages).join(
-                ', '
-              )}</p>
+             
           </li>`
       )
       .join('');
-    countryList.insertAdjacentHTML('beforeend', markup);
+    countryList.insertAdjacentHTML('beforeend', markupCountryList);
+
+    const markupCountryInfo = data
+      .map(
+        item =>
+          `<li class="iteml">
+              
+              <p class="capital">Capital: ${item.capital}</p>
+              <p class="population">Population: ${item.population}</p>
+              <p class="languages">Languages: ${Object.values(
+                item.languages
+              ).join(', ')}</p>
+          </li>`
+      )
+      .join('');
+    countryInfo.insertAdjacentHTML('beforeend', markupCountryInfo);
     console.log(data);
   }
 }
 
-// function onInput(evt) {
-//   const name = evt.currentTarget.value;
-//   console.log(name);
-
-//   fetchCountries(name) ;
-// }
-
-// function onInput(evt) {
-//   const inputValue = evt.currentTarget.value;
-//   // console.log(name);
-
-//   fetchCountries(inputValue).then(onMarkup);
-// }
-
-// function onMarkup() {
-//   countryList.innerHTML = data
-//     .map(item => {
-//       return `<li class="item">
-//               <img src= ${item.flags} alt="">
-//               <p class="name">${item.name}</p>
-//               <p class="capital">${item.capital}</p>
-//               <p class="population">${item.population}</p>
-//               <p class="languages">${item.languages}</p>
-//           </li>`;
-//     })
-//     .join('');
-//   // countryList.insertAdjacentHTML('beforeend', markup);
-// }
+function resetData() {
+  countryList.innerHTML = '';
+  countryInfo.innerHTML = '';
+}
