9c0b3d9b5cde73b2481aa5474850713378c77222
AleXKeepeR21 <106617654+AleXKeepeR21@users.noreply.github.com>
2022-11-08 21:44:55 +0200

    bagfix

--- src/index.js
+++ src/index.js
@@ -21,56 +21,64 @@ function onInput(evt) {
   }
 
   if (coutriesSearch.trim() === '') {
-    Notiflix.Notify.failure('Oops, there is no country with that name');
+    Notiflix.Notify.failure(
+      'Oops, the country name must not start with a space 😺'
+    );
     return;
   }
 
-  fetchCountries(coutriesSearch).then(onMarkup);
+  fetchCountries(coutriesSearch).then(onMarkup).catch(onFetchError);
+}
 
-  function onMarkup(data) {
+function onMarkup(data) {
+  resetData();
+
+  if (data.length > 10) {
+    Notiflix.Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
+  }
+
+  if (data.length >= 2 && data.length <= 10) {
     const markupCountryList = data
-      .map(
-        item =>
-          `<li class="item">
-              <img src= ${item.flags.svg} alt="">
-              <p class="name">${item.name.official}</p>
-             
-          </li>`
-      )
+      .map(({ flags, name }) => {
+        return `<li class="country-list__item">
+              <img src= "${flags.svg}" alt=${name.official}  width="30" height="20">
+              <h2 class="name">${name.official}</h2>
+
+          </li>`;
+      })
       .join('');
     countryList.insertAdjacentHTML('beforeend', markupCountryList);
+  }
 
+  if (data.length === 1) {
     const markupCountryInfo = data
-      .map(
-        item =>
-          `<li class="iteml">
-              
-              <p class="capital">Capital: ${item.capital}</p>
-              <p class="population">Population: ${item.population}</p>
-              <p class="languages">Languages: ${Object.values(
-                item.languages
+      .map(({ flags, name, capital, population, languages }) => {
+        return `<div class="country-info__item">
+              <li class="country-list__item">
+              <img src= "${flags.svg}" alt=${
+          name.official
+        }  width="30" height="20">
+              <h2 class="name">${name.official}</h2>
+          </li>
+              <p class="capital"><b>Capital:</b> ${capital}</p>
+              <p class="population"><b>Population:</b> ${population}</p>
+              <p class="languages"><b>Languages:</b> ${Object.values(
+                languages
               ).join(', ')}</p>
-          </li>`
-      )
+          </div>`;
+      })
       .join('');
     countryInfo.insertAdjacentHTML('beforeend', markupCountryInfo);
-    console.log(data);
-
-    // if (data.length > 10) {
-    //   Notiflix.Notify.info(
-    //     'Too many matches found. Please enter a more specific name.'
-    //   );
-    //   return;
-    // } else if (2 <= data.length >= 10) {
-    //   markupCountryList;
-    // } else {
-    //   markupCountryList;
-    //   markupCountryInfo;
-    // }
   }
 }
 
 function resetData() {
   countryList.innerHTML = '';
   countryInfo.innerHTML = '';
+}
+
+function onFetchError(data) {
+  Notiflix.Notify.failure('Oops, there is no country with that name');
 }
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,71 +1,17 @@
 export { fetchCountries };
 
 function fetchCountries(name) {
-  return (
-    fetch(
-      `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
-    )
-      // .then(response => response.json())
-      // .then(data => {
-      //   return data;
-      // });
-
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(response.status);
-        }
-        return response.json();
-      })
-      .then(data => {
-        return data;
-      })
-      .catch(error => console.log(error))
-  );
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+  )
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(data => {
+      return data;
+    })
+    .catch(error => console.log(error));
 }
-// fetchCountries('usa');
-// fetch(
-//   `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
-// )
-//   .then(response => response.json())
-//   .then(name => {
-//     const markup = name.results
-//       .map(
-//         item =>
-//           `<li class="item">
-//               <img src= ${item.flags} alt="">
-//               <p class="name">${item.name}</p>
-//               <p class="capital">${item.capital}</p>
-//               <p class="population">${item.population}</p>
-//               <p class="languages">${item.languages}</p>
-//           </li>`
-//       )
-//       .join('');
-//     countryList.insertAdjacentHTML('beforeend', markup);
-//   });
-
-// .then(response => {
-//   if (!response.ok) {
-//     throw new Error(response.status);
-//   }
-//   return response.json();
-// })
-// .then(name =>{
-//   console.log(name);
-// })
-// .catch(error => console.log(error));
-
-//   const markup = name.results
-//     .map(
-//       item =>
-//         `<li class="item">
-//               <img src= ${item.flags} alt="">
-//               <p class="name">${item.name}</p>
-//               <p class="capital">${item.capital}</p>
-//               <p class="population">${item.population}</p>
-//               <p class="languages">${item.languages}</p>
-//           </li>`
-//     )
-//     .join('');
-//   document
-//     .querySelector('.country-list')
-//     .insertAdjacentHTML('beforeend', markup);
